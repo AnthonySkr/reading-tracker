b23df548b2fe77137762f1b53964a4dafee66d06
Anthony <skrant44@gmail.com>
2025-05-12 17:15:29 +0200

    init base project

---
+++ src/views/AdminView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Admin page: for adding/editing fictions
+</script>
+
+<template>
+  <div>
+    <h1>Admin - Add your fictions 📚</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
